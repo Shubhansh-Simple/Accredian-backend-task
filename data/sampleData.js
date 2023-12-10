f01e9bfde4b0218722def6d1b2fa8b46d7e89689
shubhansh-simple <shubhansh7777@gmail.com>
2023-12-10 17:25:03 +0530

    Add sample data in our backend project for testing

---
+++ data/sampleData.js
@@ -0,0 +1,19 @@
+/*
+ * Sample data for user database
+ * It's the blueprint of the data, in our database
+ */
+
+const users = [
+  {
+    id       : 1,
+    username : 'shubhansh',
+    email    : 'shubhansh7777@gmail.com',
+    password : '123456'
+  },
+  {
+    id       : 2,
+    username : 'himansh',
+    email    : 'himansh7777@gmail.com',
+    password : '123456'
+  },
+]
