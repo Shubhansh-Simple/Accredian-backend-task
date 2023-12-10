a745befccf3d63c1307af6acfc0d268bef757cf4
shubhansh-simple <shubhansh7777@gmail.com>
2023-12-10 11:29:41 +0530

    Add ENVIRONMENT variables

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 import express from 'express';
+import dotenv  from 'dotenv' ;
 
-const port = 8000;
+// ENVIRONMENT VARIABLE
+dotenv.config();
+
+const port = process.env.PORT || 5000 ;
 
 const app = express();
 
@@ -8,7 +12,9 @@ app.get('/', ( req, res ) => {
   res.send('API is running...');
 });
 
-app.listen( port, ()=> console.log(`Server running on port ${port}`) );
+app.listen( port, 
+  () => console.log(`Server running on port ${port}`) 
+);
 
 
 
